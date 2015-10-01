15df7c3c1df42fe75fecad401938b85a37867499
Balatzar <bataprod@gmail.com>
2015-10-01 17:00:35 +0200

    init project

---
+++ base/competences.js
@@ -0,0 +1,27 @@
+/*!
+ * balthazar-cv 2.7.1
+ * https://github.com/balatzar/balthazar-cv
+ */
+
+function balthazar(competence) {
+    'use strict';
+    var i = 0;
+    while(competence[i]) {
+
+    // Compétences Informatiques
+    var HTML =                      "Maitrisé";
+    var CSS =                       "Maitrisé";
+    var JavaScript =                "En cours d'acquisition";
+    var Git, GitHub =               "Maitrisés";
+    var Scrum, Méthode Agile =      "Maitrisée";
+    var BootStrap =                 "Maitrisé";
+    var C =                         "Notions";
+
+    // Autres
+    var Montage vidéo =             "Maitrisé";
+    var Réalisation =               "Maitrisé";
+    var Batterie =                  "En cours d'acquisition";
+
+    ++i;
+    }
+};
