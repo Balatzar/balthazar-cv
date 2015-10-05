25b079727a59da487a14c13ddec77e2e800faabf
Balatzar <bataprod@gmail.com>
2015-10-05 18:22:45 +0200

    boucle title editing

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -11,10 +11,20 @@ function clicko(li) {
     var stock = li.innerHTML;
     var query = "";
     var title = "";
+    var stop = 0;
     
     for (var i = 0; stock[i] != '<'; i++) {
         query += stock[i];
     }
+    
+    for (var i = 0; i < stock.length; i++) {
+        if (stock[i] == '<' || stock[i] == '>')
+            stop++;
+        else if ((stock[i] != '<' && stop == 0) || stop == 2)
+            title += stock[i];
+    }
+    
     temp = document.querySelector(navigation[query]);
     temp.style.display = "block";
+    document.querySelector("h1").innerHTML = title;
 }
